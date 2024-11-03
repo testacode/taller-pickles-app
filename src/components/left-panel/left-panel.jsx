2524c2fa893d1d8923534929d48484dde5fb0deb
Charly Brown <bastadespammers@gmail.com>
2024-11-03 00:56:15 -0300

    feat: integrate ImagePreview component into LeftPanel for enhanced shirt image display

--- src/components/left-panel/left-panel.jsx
+++ src/components/left-panel/left-panel.jsx
@@ -1,10 +1,9 @@
-import { Box, Center, Flex, Image, Text } from "@chakra-ui/react";
-import { DataContextProvider, useDataContext } from "../../context/DataContext";
-import { SHIRT_SIDE } from "../../constants";
+import { Center, Flex, Text } from "@chakra-ui/react";
+import { useDataContext } from "../../context/DataContext";
 import { Switch } from "../ui/switch";
+import { ImagePreview } from "./image-preview";
 import { useCallback } from "react";
-import remeraEspalda from "../../assets/remera-espalda.png";
-import remeraFrente from "../../assets/remera-frente.png";
+import { SHIRT_SIDE } from "../../constants";
 
 export const LeftPanel = () => {
   const { data, updateData } = useDataContext();
@@ -17,7 +16,7 @@ export const LeftPanel = () => {
   );
 
   return (
-    <DataContextProvider>
+    <Flex className="left-panel" flexDir="column">
       <Center flexDirection="column" gap="1em">
         <Flex gap="1em">
           <Text fontWeight="bold">Elegi el lado de la estampa</Text>
@@ -38,55 +37,7 @@ export const LeftPanel = () => {
           </Text>
         </Flex>
       </Center>
-      <Flex flexDir="column" position="relative">
-        {/* Foto de la remera con los textos e imagen */}
-        {data[shirt_side].agrega_texto_arriba && (
-          <Texto texto={data[shirt_side].texto_arriba} position="top" />
-        )}
-        {data[shirt_side].imagen && <Imagen image={data[shirt_side].imagen} />}
-        <Image
-          src={isShirtSideFront(shirt_side) ? remeraFrente : remeraEspalda}
-        />
-        {data[shirt_side].agrega_texto_abajo && (
-          <Texto texto={data[shirt_side].texto_abajo} position="bottom" />
-        )}
-      </Flex>
-    </DataContextProvider>
-  );
-};
-
-const Imagen = ({ image }) => {
-  const preview = URL.createObjectURL(image);
-
-  return (
-    <Flex
-      left="50%"
-      maxWidth="30%"
-      position="absolute"
-      top="48%"
-      transform="translate(-50%, -50%)"
-    >
-      <Image src={preview} />
+      <ImagePreview />
     </Flex>
-  );
-};
-
-const Texto = ({ texto, position }) => {
-  const positions = {
-    top: { top: "30%", bottom: "auto" },
-    bottom: { top: "auto", bottom: "30%" },
-  };
-
-  return (
-    <Box
-      left="50%"
-      position="absolute"
-      transform="translate(-50%, -50%)"
-      {...positions[position]}
-    >
-      <Text fontWeight="bold" color="black">
-        {texto}
-      </Text>
-    </Box>
   );
 };
