3d07cabb05614fcc22d8ae1c9a1ef5d51199623a
Charly Brown <bastadespammers@gmail.com>
2024-11-03 01:21:19 -0300

    feat: refactor RightPanel to integrate ImageTab, ColorTab, and SettingsTab components

--- src/components/right-panel/right-panel.jsx
+++ src/components/right-panel/right-panel.jsx
@@ -1,17 +1,10 @@
-import {
-  Flex,
-  Highlight,
-  Input,
-  Separator,
-  Tabs,
-  Text,
-} from "@chakra-ui/react";
-import { Checkbox } from "../ui/checkbox";
-import { CloseButton } from "../ui/close-button";
-import { Dropzone } from "./dropzone";
+import { ImageTab } from "./image-tab";
 import { LuImage, LuPalette, LuSettings2 } from "react-icons/lu";
-import { useCallback, useMemo } from "react";
-import { useDataContext } from "../../context/DataContext";
+import { Tabs } from "@chakra-ui/react";
+// import { useDataContext } from "../../context/DataContext";
+import { useMemo } from "react";
+import { ColorTab } from "./color-tab";
+import { SettingsTab } from "./settings-tab";
 
 const TabTrigger = ({ value, icon: Icon, label }) => (
   <Tabs.Trigger value={value} justifyContent="center">
@@ -21,7 +14,9 @@ const TabTrigger = ({ value, icon: Icon, label }) => (
 );
 
 export const RightPanel = () => {
-  const { data, updateData } = useDataContext();
+  // const { data, updateData } = useDataContext();
+  // const { shirt_side } = data;
+
   const tabItems = useMemo(
     () => [
       { value: "image", icon: LuImage, label: "Imagen/Texto" },
@@ -31,26 +26,15 @@ export const RightPanel = () => {
     []
   );
 
-  const { shirt_side } = data;
-
-  const removeImage = useCallback(() => {
-    updateData({
-      [shirt_side]: {
-        ...data[shirt_side],
-        imagen: null,
-      },
-    });
-  }, [data, shirt_side, updateData]);
-
   return (
-    <Tabs.Root defaultValue="image" variant="plain">
+    <Tabs.Root defaultValue="image" variant="enclosed">
       <Tabs.List
+        alignItems="center"
         bg="bg.muted"
-        rounded="l3"
-        p="1"
         gap="1em"
         justifyContent="center"
-        alignItems="center"
+        p="1"
+        rounded="l3"
         width="100%"
       >
         {tabItems.map(({ value, icon, label }) => (
@@ -58,89 +42,14 @@ export const RightPanel = () => {
         ))}
       </Tabs.List>
       <Tabs.Content value="image">
-        <Flex justify="space-between">
-          <Text>Subi una imagen y/o agrega un texto</Text>
-          {data[shirt_side].imagen && (
-            <CloseButton
-              colorPalette="red"
-              onClick={removeImage}
-              size="2xs"
-              variant="solid"
-            />
-          )}
-        </Flex>
-        {/* Dropzone */}
-        <Dropzone />
-        <Text textStyle="xs" mb="1em" color="fg.subtle">
-          <Highlight
-            query={[".jpg", ".jpeg", ".gif", ".png"]}
-            styles={{ color: "orange.fg" }}
-          >
-            Solo podes subir imagenes con extension .jpg, .jpeg, .gif y .png
-          </Highlight>
-        </Text>
-        <Separator size="lg" mb="1em" />
-        <Flex flexDir="column" gap="0.5em">
-          <Flex gap="1em">
-            <Text>Agrega un texto arriba</Text>
-            <Checkbox
-              checked={data[shirt_side].agrega_texto_arriba}
-              onCheckedChange={({ checked }) => {
-                updateData({
-                  [shirt_side]: {
-                    ...data[shirt_side],
-                    agrega_texto_arriba: !!checked,
-                  },
-                });
-              }}
-            />
-          </Flex>
-          <Input
-            type="text"
-            placeholder="Texto de arriba"
-            value={data[shirt_side].texto_arriba}
-            onChange={(e) =>
-              updateData({
-                [shirt_side]: {
-                  ...data[shirt_side],
-                  texto_arriba: e.target.value,
-                },
-              })
-            }
-          />
-          <Flex gap="1em">
-            <Text>Agrega un texto abajo</Text>
-            <Checkbox
-              checked={data[shirt_side].agrega_texto_abajo}
-              onCheckedChange={({ checked }) => {
-                updateData({
-                  [shirt_side]: {
-                    ...data[shirt_side],
-                    agrega_texto_abajo: !!checked,
-                  },
-                });
-              }}
-            />
-          </Flex>
-          <Input
-            type="text"
-            placeholder="Texto de abajo"
-            value={data[shirt_side].texto_abajo}
-            onChange={(e) =>
-              updateData({
-                [shirt_side]: {
-                  ...data[shirt_side],
-                  texto_abajo: e.target.value,
-                },
-              })
-            }
-          />
-        </Flex>
+        <ImageTab />
       </Tabs.Content>
       <Tabs.Content value="colores">
-        Selecciona color y talle de remera
+        <ColorTab />
       </Tabs.Content>
-      <Tabs.Content value="edicion">Tunea tu remera</Tabs.Content>
+      <Tabs.Content value="settings">
+        <SettingsTab />
+      </Tabs.Content>
     </Tabs.Root>
   );
 };
