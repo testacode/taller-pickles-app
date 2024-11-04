3ca5a159e0f9ea8d650e0adde037f0c82bda20f1
Charly Brown <bastadespammers@gmail.com>
2024-11-04 20:50:24 -0300

    feat: fix import statements in main.jsx for consistency and clarity

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { ColorModeProvider } from "./components/ui/color-mode";
+import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import App from "./app.jsx";
+
 import "./index.css";
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
